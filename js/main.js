6bedec7d510a2c154e9e4296e6031c986c5df976
Dants0 <gui.dg1@hotmail.com>
2022-11-24 00:46:35 -0300

    commit changes project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#sendFormButton")
+
+
+const showAlert = () =>{
+    return alert('Cadastro realizado com sucesso!')
+}
+
+
+btn.addEventListener("click", showAlert, false)
