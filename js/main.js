2b17bfea1ef236e14e7110870e7476532a9a2e74
Dants0 <gui.dg1@hotmail.com>
2022-11-24 01:11:20 -0300

    last update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 const btn = document.querySelector("#sendFormButton")
 
+let nameValue = document.querySelector("#nome")
+let senhaValue = document.querySelector("#senha")
+let loginValue = document.querySelector("#login")
+let telefoneValue = document.querySelector("#telefone")
+let emailValue = document.querySelector("#email")
+
+
+// let arr = new Array(nameValue.value + senhaValue.value + emailValue.value + loginValue.value + telefoneValue + emailValue.value)
+
 
 const showAlert = () =>{
-    return alert('Cadastro realizado com sucesso!')
+    return alert(`Cadastro realizado com sucesso! Bem vindo ${nameValue.value}`)
 }
 
 
